72a1b1a3b4207eff4697f3643a961a1530f5313c
ChoongWan Woo <clinpsywoo@ChoongWanui-iMac.local>
2017-06-28 21:08:30 +0900

    deleted bash files

--- task_functions/rsync_data_from_INCserver_to_engram.sh
+++
@@ -1,1 +0,0 @@
-rsync -avuP /Volumes/DataStore/Wager/MPA1/data/ wani@calor.colorado.edu:/Volumes/engram/labdata/data/MPA1/Behavioral/data/
--- task_functions/rsync_to_INCserver.sh
+++
@@ -1,1 +0,0 @@
-rsync -avuP /Users/clinpsywoo/github/mpa1/task_functions_v4/ /Volumes/DataStore/Wager/MPA1/task_functions_v4/
